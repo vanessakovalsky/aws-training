fc4ef0ac9b3760c5374ab48841a221f3805f42b0
Vanessa Kovalsky <vanessa.kovalsky@free.fr>
2021-06-16 14:42:05 +0200

    Add other way of doing things

--- corrige-tp/tp-0/script_save_s3.sh
+++ corrige-tp/tp-0/script_save_s3.sh
@@ -20,6 +20,17 @@ tar -czvf $dirbackup/www-$backupdate.tar.gz /var/www/html/zf
 # sauvegarde mysql
 mysqldump --user=admin_dump --password=Dump2021* --all-databases | /usr/bin/gzip > $dirbackup/mysqldump-$backupdate.sql.gz
 
+# Connexion AWS depuis fichier CSV
+
+aws configure import --csv file://credentials.csv
+
+# Connexion AWS depuis variable d'environnement 
+# Définir les variables d'environnement 
+# AWS_ACCESS_KEY_ID 
+# AWS_SECRET_ACCESS_KEY 
+# AWS_DEFAULT_REGION 
+# De fait aws va récupérer les valeurs de ces variables et n'a pas besoin d'etre configurer
+
 # Création du bucket
 bucketname=my-first-backup-bucket-vanessa
 
@@ -55,3 +66,10 @@ aws s3 ls $bucketname/ | while read -r line;
        fi
 
        done; 
+
+# Autre moyen : avec l'ajout d'une règle depuis un fichier JSON
+# aws s3api put-bucket-lifecycle-configuration --bucket $bucketname --lifecycle-configuration file://lifecycle.json
+
+## Copier avec l'accès peu fréquent
+
+aws s3 cp $dirbackup/www-$backupdate.tar.gz s3://$bucketname/ --storage-class STANDARD_IA
