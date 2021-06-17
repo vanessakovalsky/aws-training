b02e97acad3a1986d102020f8d4a96e73038973f
Vanessa Kovalsky <vanessa.kovalsky@free.fr>
2021-06-17 11:53:30 +0200

    Add deployment to script

--- corrige-tp/tp-1/script_install_wordpress_ec2.sh
+++ corrige-tp/tp-1/script_install_wordpress_ec2.sh
@@ -153,4 +153,28 @@ INSTANCE_IP=$(aws ec2 describe-instances \
     --output text )
 
 # Connexion SSH dans l'instance 
+ssh -o "StrictHostKeyChecking no" $INSTANCE_IP
 ssh -i $KEY_NAME.pem ec2-user@$INSTANCE_IP
+
+# Déploiement de l'application
+
+#sudo yum update -y
+#sudo amazon-linux-extras enable php7.4
+#sudo yum install -y httpd
+#sudo yum install -y mariadb-server
+#sudo yum install -y php-cli php-pdo php-fpm php-json php-mysqlnd
+#sudo service mariadb start
+#sudo service httpd start
+#mysqladmin -u root create blog
+#mysql_secure_installation
+
+#cd /var/www/html
+#sudo wget http://wordpress.org/latest.tar.gz
+#sudo tar -xzvf latest.tar.gz
+#sudo mv wordpress/* .
+#sudo rm -rf wordpress
+
+exit
+
+echo "déploiement de l'application terminée"
+echo "Veuillez effectuer les dernières étapes sur http://"$INSTANCE_IP
